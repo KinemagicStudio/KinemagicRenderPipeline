c0490c3745b9b28693677e7a5bf8dda0bf92d2b5
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2026-01-25 11:14:03 +0900

    feat: Add SetActive method to PostProcessComponent

--- src/UniversalRenderPipeline/Runtime/PostProcessComponent.cs
+++ src/UniversalRenderPipeline/Runtime/PostProcessComponent.cs
@@ -6,5 +6,6 @@ namespace Kinemagic.Rendering.Universal
     {
         public abstract string GetName();
         public abstract bool IsActive();
+        public abstract void SetActive(bool value);
     }
 }
