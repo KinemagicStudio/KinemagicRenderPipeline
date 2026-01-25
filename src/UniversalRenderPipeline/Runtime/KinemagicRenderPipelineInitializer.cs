c5d171a938112bfadc0b7cc4e4cae3e44ed90ecd
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2026-01-25 11:27:39 +0900

    feat: Add KinemagicRenderPipelineInitializer class with render pass factory registration

---
+++ src/UniversalRenderPipeline/Runtime/KinemagicRenderPipelineInitializer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Kinemagic.Rendering.Universal
+{
+    public static class KinemagicRenderPipelineInitializer
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void RegisterRenderPassFactories()
+        {
+            DebugLogger.Log("<color=lime>[KinemagicRenderPipelineInitializer] Register render pass factories.</color>");
+
+            var factoryProvider = PostProcessRenderPassFactoryProvider.Instance;
+
+            // factoryProvider.Register<ScreenEdgeColor>(_ =>
+            // {
+            //     return new ScreenEdgeColorRenderPass();
+            // });
+        }
+    }
+}
